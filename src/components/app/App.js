2765c2bc17235d46cd6888d3dc59ead61bbd26de
AlenaGM <alena.guillaume4@gmail.com>
2022-05-02 19:53:58 +0200

    update classes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,16 +5,18 @@ import plans from '../../resources/data.json';
 function App() {
   return (
     <div className="app">
-      {
-        plans.map((plan) =>
-        <Card
-        price={plan.price}
-        speed={plan.speed}
-        description={plan.description}
-        darkColor={plan.darkColor}
-        lightColor={plan.lightColor}/>
-        )
-      }
+      <div className="app__container">
+        {
+          plans.map((plan) =>
+          <Card
+          price={plan.price}
+          speed={plan.speed}
+          description={plan.description}
+          darkColor={plan.darkColor}
+          lightColor={plan.lightColor}/>
+          )
+        }
+      </div>
     </div>
   );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,11 +4,11 @@ import './card.css';
 const Card = (props) => {
 
     return(
-        <div className="appCard">
+        <div className="app__card">
             <div className={props.darkColor}>Безлимитный {props.price}</div>
-            <div className={props.lightColor}>руб<span className="card-price">{props.price}</span>/мес</div>
-            <div className="card-speed">до {props.speed} Мбит/сек</div>
-            <div className="card-description">{props.description}</div>
+            <div className={props.lightColor}>руб<span>{props.price}</span>/мес</div>
+            <div>до {props.speed} Мбит/сек</div>
+            <div>{props.description}</div>
         </div>
     )
 
