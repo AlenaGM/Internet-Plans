4c74622ec9f225e51155757ba680e875997790e7
AlenaGM <alena.guillaume4@gmail.com>
2022-05-03 00:03:09 +0200

    impr

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,23 +4,12 @@
 const Card = (props) => {
 
     return(
-        <>
-        {props.isAction ?
-            <div  className="app__card card_action">
-                <div>Безлимитный {props.price}</div>
-                <div><p className="up">руб</p><span>{props.price}</span>/мес</div>
-                <div>до {props.speed} Мбит/сек</div>
-                <div>{props.description}</div>
-            </div>
-            :
-            <div  className="app__card">
-                <div>Безлимитный {props.price}</div>
-                <div><p className="up">руб</p><span>{props.price}</span>/мес</div>
-                <div>до {props.speed} Мбит/сек</div>
-                <div>{props.description}</div>
-            </div>
-        }
-        </>
+        <div  className={props.isAction ? 'app__card card_action' : 'app__card'}>
+            <div>Безлимитный {props.price}</div>
+            <div><p className="up">руб</p><span>{props.price}</span>/мес</div>
+            <div>до {props.speed} Мбит/сек</div>
+            <div>{props.description}</div>
+        </div>
     )
 
 }
