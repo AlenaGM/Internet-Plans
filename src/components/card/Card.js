e7cd8e23861ca9cf8f22c2135ffca5a59636ed7d
AlenaGM <alena.guillaume4@gmail.com>
2022-05-06 14:55:22 +0200

    upd

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,15 +3,15 @@ import { useState } from 'react';
 
 const Card = ({price,speed,description,isSelected,onClick}) => {
 
-    const [tag, setTag] = useState(false);
+    //const [tag, setTag] = useState(false);
 
     const handleClick = () => {
-        setTag(!tag);
+        //setTag(!tag);
         onClick(price);
     }
 
     return(
-        <div  className={isSelected? `app__card ${tag && 'card_action'}` : 'app__card'} onClick={handleClick}>
+        <div  className={isSelected? 'app__card card_action' : 'app__card'} onClick={handleClick}>
             <div>Безлимитный {price}</div>
             <div><p className="up">руб</p><span>{price}</span>/мес</div>
             <div>до {speed} Мбит/сек</div>
