aa905e891f5bab3c5d9defbe83486dcae1988fc3
AlenaGM <alena.guillaume4@gmail.com>
2022-09-27 11:34:18 +0200

    upd en

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,20 +1,22 @@
-const Card = ({price,speed,description,isSelected,onClick}) => {
+const Card = ({ price, speed, description, isSelected, onClick }) => {
+  const handleClick = () => {
+    onClick(price);
+  };
 
-    const handleClick = () => {
-        onClick(price);
-    }
-
-    return(
-        <div  className={isSelected? 'app__card card_action' : 'app__card'} onClick={handleClick}>
-            <div>Безлимитный {price}</div>
-            <div><p className="up">руб</p><span>{price}</span>/мес</div>
-            <div>до {speed} Мбит/сек</div>
-            <div>{description}</div>
-        </div>
-    )
-
-}
+  return (
+    <div
+      className={isSelected ? "app__card card_action" : "app__card"}
+      onClick={handleClick}
+    >
+      <div>Illimité {price}</div>
+      <div>
+        <p className="up">€</p>
+        <span>{price}</span>/month
+      </div>
+      <div>up to {speed} Mbits/s </div>
+      <div>{description}</div>
+    </div>
+  );
+};
 
 export default Card;
-
-
